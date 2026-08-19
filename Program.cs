b7d0cd9082c08391a1b909c518d950957cffa1cb
Utso Roy <royutso003@gmail.com>
2026-08-19 22:33:17 +0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Test
+{
+    public static void Main(string[] args)
+    {
+        //Variable Declaration
+        string name = "Utso";
+        Console.WriteLine(name);
+        char firstLetter = name[0];
+        Console.WriteLine(firstLetter);
+        double number = 3.14;
+        Console.WriteLine(number);
+        int age = 25;
+        Console.WriteLine(age);
+        // int a = b = c = 10;  multiple variable declaration 
+        // const string varcityName = "Uttara University"; Constant variable
+        string varcityName = "Uttara University";
+        Console.WriteLine(varcityName);
+        varcityName = "Dhaka University";
+        Console.WriteLine(varcityName);
+
+    }
+}
+
