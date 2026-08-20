8c4e0aa3595b257c174277083adaec96bcd75d16
Utso Roy <royutso003@gmail.com>
2026-08-20 13:43:12 +0600

    practice C# type checking

--- Program.cs
+++ Program.cs
@@ -4,21 +4,44 @@ class Test
     public static void Main(string[] args)
     {
         //Variable Declaration
-        string name = "Utso";
-        Console.WriteLine(name);
-        char firstLetter = name[0];
-        Console.WriteLine(firstLetter);
-        double number = 3.14;
-        Console.WriteLine(number);
-        int age = 25;
-        Console.WriteLine(age);
+        // string name = "Utso";
+        // Console.WriteLine(name);
+        // char firstLetter = name[0];
+        // Console.WriteLine(firstLetter);
+        // double number = 3.14;
+        // Console.WriteLine(number);
+        // int age = 25;
+        // Console.WriteLine(age);
         // int a = b = c = 10;  multiple variable declaration 
         // const string varcityName = "Uttara University"; Constant variable
-        string varcityName = "Uttara University";
-        Console.WriteLine(varcityName);
-        varcityName = "Dhaka University";
-        Console.WriteLine(varcityName);
+        // string varcityName = "Uttara University";
+        // Console.WriteLine(varcityName);
+        // varcityName = "Dhaka University";
+        // Console.WriteLine(varcityName);
 
+        // Lean to get user input 
+        // Console.WriteLine("Write Your Name");
+        // string StudentName = Console.ReadLine();
+        // Console.WriteLine("My Name Is " + StudentName);
+
+        // int a = 10;
+        // int b = 3;
+
+        // double result = (double)a / b;
+        // Console.WriteLine(result.ToString("F3"));
+
+
+
+        // int number1;
+        // int number2;
+
+        // Console.WriteLine("Please Write First Number");
+        // number1 = int.Parse(Console.ReadLine());
+        // Console.WriteLine("Please Write Second Number");
+        // number2 = int.Parse(Console.ReadLine());
+        // int sum = number1 + number2;
+        // Console.WriteLine("Total Sum " + sum);
+        // Console.WriteLine("Type" + sum.GetType());
     }
 }
 
